78b5bf5d6e9180011a2332a72997c098b0b844bc
TalSinay <talsinay1000@gmail.com>
2023-05-29 16:38:32 +0300

    express

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+
 const port = process.env.PORT || 3000;
 const app = express();
 
 
 app.get('/', (req, res) => {
     console.log('new request has arrived');
-    res.send("<h1 style='color: blue'>hello from the server main page </h1>");
+    res.send("<h1 style='color: blue'>hello from the server main </h1>");
 });
 
 app.listen(port,()=>
 {
-    console.log("server is up and run is=n port" + port) ;
-})
+    console.log("server is up and run in port" + port) ;
+});
