bf5390496eaf4a8fd50e58593276ed7cca8ea524
TalSinay <talsinay1000@gmail.com>
2023-05-29 19:19:07 +0300

    h2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   console.log('new request has arrived');
-  res.send("<h1 style='color: blue'>hello from the server main </h1>");
+  res.send("<h1 style='color: blue'>hello from the server main </h1> <br></br> <h2 style='color:red'>this is the register page </h2>");
 });
 
 app.listen(port, () => {
