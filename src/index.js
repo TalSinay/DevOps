82183f9df882e08ad4c419fc414dbbf891d98f7c
TalSinay <talsinay1000@gmail.com>
2023-05-29 18:07:28 +0300

    linter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
-
 app.get('/', (req, res) => {
-    console.log('new request has arrived');
-    res.send("<h1 style='color: blue'>hello from the server main </h1>");
+  console.log('new request has arrived');
+  res.send("<h1 style='color: blue'>hello from the server main </h1>");
 });
 
-app.listen(port,()=>
-{
-    console.log("server is up and run in port" + port) ;
+app.listen(port, () => {
+  console.log('server is up and run in port', port);
 });
---
+++ tests/Arrays.spec.js
@@ -0,0 +1,9 @@
+const x = require ('chai');
+
+describe('Arrays',()=>{
+    describe('#sort',()=>{
+        it('calculator',()=>{
+            x.expect(5-2).to.be.eq(3);
+        })
+    })
+})
